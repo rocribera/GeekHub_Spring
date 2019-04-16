af5f70303a899f5eece0fbb37569898e17c6d106
Oriol <ori_tossa_98@hotmail.com>
2019-04-16 12:43:15 +0200

    Create POST /games/{id}/posts

--- src/main/java/org/udg/pds/springtodo/controller/GameController.java
+++ src/main/java/org/udg/pds/springtodo/controller/GameController.java
@@ -1,12 +1,16 @@
 package org.udg.pds.springtodo.controller;
 
+import com.fasterxml.jackson.databind.ser.Serializers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.udg.pds.springtodo.entity.Game;
 import org.udg.pds.springtodo.entity.Post;
 import org.udg.pds.springtodo.service.GameService;
+import org.udg.pds.springtodo.service.PostService;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.util.Collection;
 
 @RequestMapping(path="/games")
@@ -15,6 +19,9 @@ public class GameController extends BaseController{
 
     @Autowired
     GameService gameService;
+
+    @Autowired
+    PostService postService;
 
     @GetMapping(path="/{id}")
     public Game getGame(HttpSession session,
@@ -39,6 +46,15 @@ public class GameController extends BaseController{
         return gameService.getListPosts(id);
     }
 
+    @PostMapping(path="/{id}/posts", consumes = "application/json")
+    public String uploadPost(HttpSession session, @PathVariable("id") Long id, @Valid @RequestBody UploadPost up){
+
+        Long userId = getLoggedUser(session);
+
+        postService.createPost(up.title,true,up.description,userId,id);
+        return BaseController.OK_MESSAGE;
+    }
+
     @DeleteMapping(path="/{id}")
     public String deleteGame(HttpSession session,
                              @PathVariable("id") Long gameId) {
@@ -47,5 +63,12 @@ public class GameController extends BaseController{
 
         gameService.crud().deleteById(gameId);
         return BaseController.OK_MESSAGE;
+    }
+
+    static class UploadPost {
+        @NotNull
+        public String title;
+        @NotNull
+        public String description;
     }
 }
