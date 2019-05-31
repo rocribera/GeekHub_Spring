24941067e98f0456433c6137ffacde1d23197920
Roc <rocribera@hotmail.com>
2019-05-31 19:02:16 +0200

    GetFollowersList

--- src/main/java/org/udg/pds/springtodo/controller/PostController.java
+++ src/main/java/org/udg/pds/springtodo/controller/PostController.java
@@ -3,9 +3,11 @@ package org.udg.pds.springtodo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.udg.pds.springtodo.entity.Post;
+import org.udg.pds.springtodo.entity.User;
 import org.udg.pds.springtodo.service.PostService;
 
 import javax.servlet.http.HttpSession;
+import java.util.Collection;
 
 @RequestMapping(path="/posts")
 @RestController
@@ -18,6 +20,13 @@ public class PostController extends BaseController{
     public Post getPost(HttpSession session, @PathVariable("id") Long postId){
         getLoggedUser(session);
         return postService.getPost(postId);
+    }
+
+    @GetMapping(path="/{id}/followers")
+    public Collection<User> getFollowers(HttpSession session, @PathVariable("id") Long postId){
+        Long userId = getLoggedUser(session);
+
+        return postService.getFollowers(userId, postId);
     }
 
     @DeleteMapping(path="/{id}")
